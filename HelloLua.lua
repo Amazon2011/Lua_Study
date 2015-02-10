a12ae856aec6b6fe790528eab97ea5d33eb9af4a
Amazon2011 <chenyongtao2011@gmail.com>
2015-02-10 19:03:40 +0800

    Update study file

--- HelloLua.lua
+++ HelloLua.lua
@@ -2,4 +2,19 @@ function printSomething(something)
 	print(something)
 end
 
-printSomething(string.lower("ABC"))
+--printSomething(string.lower("ABC"))
+
+--[[local someTable = {}
+
+for i = 1, 10 do
+	table.insert(someTable, {a = i})
+end
+
+print(someTable)]]
+
+FILE_NAME = "d:/abc/1.txt"
+
+file = io.open(FILE_NAME, "w")
+assert(file)
+file:write("abc")
+file:close()
